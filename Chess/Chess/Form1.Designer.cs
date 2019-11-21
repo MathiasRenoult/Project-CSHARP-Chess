d3624b4a506e0919d210c0de7d916fe0bdffb9e1
MathiasRenoult <47390290+MathiasRenoult@users.noreply.github.com>
2019-11-21 14:28:42 +0100

    Update IHM

--- Chess/Chess/Form1.Designer.cs
+++ Chess/Chess/Form1.Designer.cs
@@ -28,13 +28,94 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.txtMail = new System.Windows.Forms.TextBox();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.lblMail = new System.Windows.Forms.Label();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(169, 150);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(186, 20);
+            this.txtPassword.TabIndex = 3;
+            // 
+            // txtMail
+            // 
+            this.txtMail.Location = new System.Drawing.Point(169, 124);
+            this.txtMail.Name = "txtMail";
+            this.txtMail.Size = new System.Drawing.Size(186, 20);
+            this.txtMail.TabIndex = 4;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(383, 148);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(75, 23);
+            this.btnLogin.TabIndex = 6;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblMail
+            // 
+            this.lblMail.AutoSize = true;
+            this.lblMail.Location = new System.Drawing.Point(49, 126);
+            this.lblMail.Name = "lblMail";
+            this.lblMail.Size = new System.Drawing.Size(67, 13);
+            this.lblMail.TabIndex = 7;
+            this.lblMail.Text = "Mail Address";
+            this.lblMail.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Location = new System.Drawing.Point(49, 153);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(53, 13);
+            this.lblPassword.TabIndex = 8;
+            this.lblPassword.Text = "Password";
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.Location = new System.Drawing.Point(193, 9);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(69, 25);
+            this.lblTitle.TabIndex = 9;
+            this.lblTitle.Text = "Chess";
+            this.lblTitle.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(470, 232);
+            this.Controls.Add(this.lblTitle);
+            this.Controls.Add(this.lblPassword);
+            this.Controls.Add(this.lblMail);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.txtMail);
+            this.Controls.Add(this.txtPassword);
+            this.Name = "Form1";
+            this.Text = "Chess.exe";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.TextBox txtMail;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.Label lblMail;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.Label lblTitle;
     }
 }
 
--- Chess/Chess/Form1.cs
+++ Chess/Chess/Form1.cs
@@ -16,5 +16,35 @@ namespace Chess
         {
             InitializeComponent();
         }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            txtMail.Text
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
     }
 }
